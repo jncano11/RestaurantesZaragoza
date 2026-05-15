59b6c751de4daedf53613f34f6910606ab865a32
samuprogramming25 <samuprogramming25@gmail.com>
2026-05-15 13:12:13 +0200

    based de datos

--- restaurantes_zaragoza.sql
+++ restaurantes_zaragoza.sql
@@ -377,4 +377,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-13 11:04:10
+-- Dump completed on 2026-05-13 11:01:43
